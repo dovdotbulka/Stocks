74a01e69e7a6dfd40fc69ba5cf9b6a60c3dd37b2
Dov Bulka <dov.bulka@gmail.com>
2021-05-23 21:18:33 -0400

    add tickers.csv

--- Hello_World.py
+++ Hello_World.py
@@ -2,37 +2,63 @@ import argparse
 import datetime as dt
 import pandas as pd
 import pandas_datareader.data as web
+import os
 
-def run(start, end, ticker, v):
-    try:
-        df = web.DataReader(ticker, 'yahoo', start, end)
-    except:
-    	print("Bad ticker..." + ticker)
-    	return
+def ticker_universe(ticker_file, verbose):
+	my_col = 'Universe'
+	tickers = []
+	if ticker_file != '':
+		df = pd.read_csv(ticker_file)
+		cols = df.columns.tolist()
+		if my_col not in cols:
+			print("ticker_universe()::{} column not found in {}".format(my_col, cols))
+			return tickers
+		col = df[my_col]
+		col.dropna(inplace=True)
 
-    print(df.head())
-    df.to_csv('{}.csv'.format(ticker))
+		tickers = col.tolist()
+		if verbose > 1:
+			print("ticker_universe()::column {}".format(col))
+			print("ticker_universe()::{} stock tickers...{}".format(my_col, tickers))
+		
+
+def run(start, end, ticker, ticker_file, verbose):
+	ticker_universe(ticker_file, verbose)
+	if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+		try:
+			df = web.DataReader(ticker, 'yahoo', start, end)
+		except:
+			print("Bad ticker..." + ticker)
+			return
+
+		print(df.head())
+		df.to_csv('stock_dfs/{}.csv'.format(ticker))
+	else:
+		print('stock_dfs/{}.csv already exists'.format(ticker))
+
+    
 
 def main():
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--start", default='2005-01-01', help="starting date")
-    parser.add_argument("-e", "--end", default='2020-12-31', help="end date")
-    parser.add_argument("-t", "--ticker", default='TSLA', help="stock ticker")
-    parser.add_argument("-v", "--verbosity", action="count", default=0,
-                        help="increase output verbosity")
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-s", "--start", default='2001-01-01', help="starting date")
+	parser.add_argument("-e", "--end", default='2021-05-01', help="end date")
+	parser.add_argument("-t", "--ticker", default='TSLA', help="stock ticker")
+	parser.add_argument("-f", "--file", default='', help="tickers file")
+	parser.add_argument("-v", "--verbosity", action="count", default=0,
+	                    help="increase output verbosity")
     
     
-    args = parser.parse_args()
+	args = parser.parse_args()
 
-    if args.verbosity > 1:
-        print("starting date={},  end date={}".format(args.start,args.end))
-        if args.file != '':
-            print("ticker file... ", args.file)
+	if args.verbosity > 1:
+		print("starting date={},  end date={}".format(args.start,args.end))
+		if args.file != '':
+			print("ticker file... ", args.file)
         
-    run(args.start, args.end, args.ticker, args.verbosity)
+	run(args.start, args.end, args.ticker, args.file, args.verbosity)
 
 
 if __name__ == "__main__":
-    main()
+	main()
 
--- README.md
+++ README.md
@@ -7,3 +7,19 @@ You provide a stock ticker, start data and end date, and the program
 will fetch closing prices for that particular ticker and store them
 in a csv file
 
+Start with accepting one ticker from the CLI, pulling quotes for the given period.
+produce a csv file and store it in a separate directory, stock_dfs/<ticker>.csv
+
+Later, extend the program to read a list of tickers from elsewhere, maybe a tickers.csv
+file with the first column called Universe, for the universe of stocks?
+
+Develop the ability to pull up and join all the csv files in the stock_dfs dir to a single
+data frame for all tickers. The tickers will be the columns and the rows are the dates.
+
+1. Populate stock_dfs with ticker csv files. Done.
+1a. Should I skip pulling quotes if the csv already exists? Yes. Done.
+2. Accept a group of tickers from a file
+3. Roll up all tickers to a single df
+4. What dates should I go for? Twenty years? 2001-01-01 to now? Yes. Done.
+5. Rename Hello_World.py to something more appropriate
+
